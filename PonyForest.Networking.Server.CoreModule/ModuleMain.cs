ecc6ea292eb6a1245223695e5253fd91c0719afd
Lyrapuff <lyrapuff@gmail.com>
2020-07-03 12:24:51 +0300

    Core module

---
+++ PonyForest.Networking.Server.CoreModule/ModuleMain.cs
@@ -0,0 +1,30 @@
+﻿using PonyForestServer.Core.Models.Messages;
+using PonyForestServer.Core.Modules;
+using PonyForestServer.Core.Modules.Attributes;
+using PonyForestServer.Core.Services;
+using PonyForestServer.Core.Tools;
+
+namespace PonyForest.Networking.Server.CoreModule
+{
+    public class ModuleMain : ServerModule
+    {
+        private readonly Logger _logger;
+        private readonly IWorld _world;
+
+        public ModuleMain(ILoggerProvider loggerProvider, IWorld world)
+        {
+            _logger = loggerProvider.GetLogger("CoreModule");
+            _world = world;
+        }
+
+        [MessageHandler(typeof(PlayerConnectionMessage))]
+        public PlayerConnectionMessage PlayerConnection(PlayerConnectionMessage message)
+        {
+            _world.AddPlayer(message.Sender);
+            
+            _logger.LogInformation($"Welcome, {message.Sender.SteamId}!");
+
+            return message;
+        }
+    }
+}
